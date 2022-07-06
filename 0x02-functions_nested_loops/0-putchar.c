68a02456b31c7874fc82d23233635df03afb10b3
Elijah Murimi <elijahmurimi40@gmail.com>
2022-07-06 11:37:56 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,15 @@
 
 int main(void)
 {
-  _putchar('E');
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
   
   return (0);
 }
