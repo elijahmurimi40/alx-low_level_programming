2fafd0293a3d345ff3a39c7cfb4db5757371daa6
elijahmurimi40 <elijahmurimi40@gmail.com>
2022-07-06 02:09:43 -0700

    Updte 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
-  * main prints '_putchar'
-  *
-  * return 0 for success
+  * main - prints _putchar, followed by a new line
+  * Return: Always 0 (Success)
   */
 int main(void)
 {
