18473a2af870f3eb2c7fe0e4fa8daf4845bf94ac
Elijah Murimi <elijahmurimi40@gmail.com>
2022-07-12 16:33:33 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,16 +4,16 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (char[i] != '\0')
+	while (s[i] != '\0')
 	{
 		i += 1;
 	}
 
 	while (i >= 0)
 	{
-		_putchar(char[i]);
+		_putchar(s[i]);
 		i -= 1;
 	}
 
-	_putvhar('\n');
+	_putchar('\n');
 }
