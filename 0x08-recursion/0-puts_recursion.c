5e8c20eac7c2d52a8e230234e88413a9e067b999
elijahmurimi40 <elijahmurimi40@gmail.com>
2022-07-20 00:57:37 -0700

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,17 @@
+#include "main.h"
 
+void _puts_recursion_helper(char *s int n)
+{
+	if (s[n] != '\0')
+	{
+		_putchar(s[n]);
+		_puts_recursion_helper(s, n += 1)
+	}
+
+	_putchar('\n');
+}
+
+void _puts_recursion(char *s)
+{
+	_puts_recursion_helper(s, 0);
+}
