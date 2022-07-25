26e2d0c7563a467ead1e8a933dc7696cd76013f8
elijahmurimi40 <elijahmurimi40@gmail.com>
2022-07-25 00:16:31 -0700

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,1 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
 
+char *create_array(unsigned int size, char c) {
+	if (size == 0) return NULL;
+
+	char *arr;
+	int n;
+
+	arr = malloc(sizeof(char) * size);
+	n = size;
+
+	while (n >= 0) {
+		*(arr + n) = c;
+		n -= 1;
+	}
+	return *arr;
+}
